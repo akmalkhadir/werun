97c91aa615fb5962a704eb80408c8a96ea1f29b2
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-18 16:47:16 +0000

    change font to rubik

--- src/Components/Root.js
+++ src/Components/Root.js
@@ -2,12 +2,31 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import App from '../App'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+    fontFamily: [
+      'Rubik',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(',')
+  }
+})
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <MuiThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </MuiThemeProvider>
   </Provider>
 )
 
