b6ca60a5715fe31ed5fac06a5d20a1732ea0c7df
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-28 15:04:38 +0000

    basic discover ui done

--- src/Containers/RunsContainer.js
+++ src/Containers/RunsContainer.js
@@ -1,25 +1,31 @@
 import React from 'react'
 import RunCard from '../Components/RunCard'
-import { Grid, withStyles } from '@material-ui/core'
+import { Grid, withStyles, Paper } from '@material-ui/core'
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
     padding: 8 * 3
+  },
+  paper: {
+    height: '10vh'
   }
 })
 
 function RunsContainer ({ classes, children, dir, runs, currentUserId }) {
   return (
-    <div component='div' dir={dir} className={classes.root}>
-      <Grid container spacing={16}>
-        {runs.map(run =>
-          <Grid item key={run.id} >
-            <RunCard run={run} currentUserId={currentUserId} />
-          </Grid>
-        )}
-      </Grid>
-    </div>
+    <>
+      <Paper className={classes.paper} elevation={0} />
+      <div component='div' dir={dir} className={classes.root}>
+        <Grid container spacing={16}>
+          {runs.map(run => (
+            <Grid item key={run.id}>
+              <RunCard run={run} currentUserId={currentUserId} />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </>
   )
 }
 
