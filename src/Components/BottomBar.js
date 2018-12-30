8657f3f2b71561eb83a1c6b44c313f4261e79054
mal <1881633+akmalkhadir@users.noreply.github.com>
2018-12-30 19:43:09 +0000

    add navigation to discover runs

--- src/Components/BottomBar.js
+++ src/Components/BottomBar.js
@@ -4,7 +4,7 @@ import {
   BottomNavigationAction,
   withStyles
 } from '@material-ui/core'
-import { Event, People, Person } from '@material-ui/icons'
+import { Event, Person, Search } from '@material-ui/icons'
 import { Link } from 'react-router-dom'
 
 const styles = {
@@ -29,10 +29,15 @@ class BottomBar extends Component {
      return <Link to={`/runners/${currentUserId}`} {...props} />
  }
 
+  redirectToDiscover = (props) => {
+    return <Link to='/runs/search' {...props} />
+  }
+
 
   render () {
     const { classes } = this.props
     const { value} = this.state
+    const { redirectToDiscover, redirectToRuns } = this
 
     return (
       <BottomNavigation
@@ -41,8 +46,8 @@ class BottomBar extends Component {
         className={classes.root}
         showLabels
       >
-        <BottomNavigationAction component={this.redirectToRuns} label='My Runs' icon={<Event />} />
-        <BottomNavigationAction label='Groups' icon={<People />} />
+        <BottomNavigationAction component={redirectToRuns} label='My Runs' icon={<Event />} />
+        <BottomNavigationAction component={redirectToDiscover} label='Discover' icon={<Search />} />
         <BottomNavigationAction label='Profile' icon={<Person />} />
       </BottomNavigation>
     )
