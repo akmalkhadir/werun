ec258ac0d6be2b6a6e50a8bcf9c0b9a0a6828d95
mal <1881633+akmalkhadir@users.noreply.github.com>
2019-01-02 10:50:15 +0000

    point to remote API

--- src/API.js
+++ src/API.js
@@ -1,29 +1,32 @@
 export default class Api {
   static getRunnerRuns = id =>
-    fetch(`http://localhost:3001/api/v1/runners/${id}`).then(resp =>
-      resp.json()
+    fetch(`https://werun-api-eu.herokuapp.com/api/v1/runners/${id}`).then(
+      resp => resp.json()
     )
 
   static getAllRuns = () =>
-    fetch(`http://localhost:3001/api/v1/runs`).then(resp => resp.json())
+    fetch(`https://werun-api-eu.herokuapp.com/api/v1/runs`).then(resp =>
+      resp.json()
+    )
 
   static getARun = id =>
-    fetch(`http://localhost:3001/api/v1/runs/${id}`).then(resp => resp.json())
+    fetch(`https://werun-api-eu.herokuapp.com/api/v1/runs/${id}`).then(resp =>
+      resp.json()
+    )
 
   static createNewRun = data =>
-    fetch(`http://localhost:3001/api/v1/runs`, {
+    fetch(`https://werun-api-eu.herokuapp.com/api/v1/runs`, {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
         'Content-Type': 'application/json'
       }
-    })
-      .then(res => res.json())
-     // .then(response => console.log('Success:', response))
-     // .catch(error => console.error('Error:', error))
+    }).then(res => res.json())
+  // .then(response => console.log('Success:', response))
+  // .catch(error => console.error('Error:', error))
 
   static joinARun = userAndRunId =>
-    fetch(`http://localhost:3001/api/v1/runners_runs/`, {
+    fetch(`https://werun-api-eu.herokuapp.com/api/v1/runners_runs/`, {
       method: 'POST',
       body: JSON.stringify(userAndRunId),
       headers: {
@@ -35,7 +38,7 @@ export default class Api {
       .catch(error => console.error('Error:', error))
 
   static unJoinARun = userAndRunId =>
-    fetch(`http://localhost:3001/api/v1/runners_runs/`, {
+    fetch(`https://werun-api-eu.herokuapp.com/api/v1/runners_runs/`, {
       method: 'DELETE',
       body: JSON.stringify(userAndRunId),
       headers: {
